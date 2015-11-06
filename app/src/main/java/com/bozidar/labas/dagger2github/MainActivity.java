1aaf67252752d1c67ca1c2d0037ce07e209668f5
bozidar <bozidar.labas@hotmail.com>
2015-11-06 17:13:44 +0100

    Added Butter Knife library

--- app/src/main/java/com/bozidar/labas/dagger2github/MainActivity.java
+++ app/src/main/java/com/bozidar/labas/dagger2github/MainActivity.java
@@ -1,13 +1,25 @@
 package com.bozidar.labas.dagger2github;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.main_checkbox)
+    CheckBox checkBox;
+
+    @Bind(R.id.main_tv_text)
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
