a8fb90a638fa2e175d9ae63e92c436d50b5334f1
bozidar <bozidar.labas@hotmail.com>
2015-11-06 17:05:10 +0100

    Prepared project structure

---
+++ app/src/main/java/com/bozidar/labas/dagger2github/DaggerApplication.java
@@ -0,0 +1,14 @@
+package com.bozidar.labas.dagger2github;
+
+import android.app.Application;
+
+/**
+ * Created by Bozidar on 06.11.2015..
+ */
+public class DaggerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
